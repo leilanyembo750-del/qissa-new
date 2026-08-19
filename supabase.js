93a2091200f2e44bf028230845bf152a6f200ec8
leilanyembo750-del <leilanyembo750@gmail.com>
2026-08-19 22:25:03 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL ="https://pehpwrlvafreyiiqauci.supabase.co"
+const SUPABASE_PUBLISHABLE_KEY ="sb_publishable_wtfgfwA_r1PrKFmQoUWkug_jO9tbu53"
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
